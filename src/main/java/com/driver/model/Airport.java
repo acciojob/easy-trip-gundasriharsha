7f337d1d9975e51874e2098c3314983b89ba3e17
gundasriharsha <harshagunda2002@gmail.com>
2023-05-27 15:35:40 +0530

    done

--- src/main/java/com/driver/model/Airport.java
+++ src/main/java/com/driver/model/Airport.java
@@ -11,6 +11,7 @@ public class Airport {
     private City city;  //GIVEN : There will be only 1 airport in 1 city
 
     public Airport() {
+
     }
 
     public Airport(String airportName, int noOfTerminals, City city) {
